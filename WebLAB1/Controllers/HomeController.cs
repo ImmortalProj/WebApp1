fb9f29478138e12b269369f2c2a3eab11ff19dc9
ImmortalProj <86962310+ImmortalProj@users.noreply.github.com>
2021-10-23 15:43:02 +0300

    WebApp1
    WebApp1

--- WebLAB1/Controllers/HomeController.cs
+++ WebLAB1/Controllers/HomeController.cs
@@ -33,5 +33,17 @@ namespace WebLAB1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Download()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
